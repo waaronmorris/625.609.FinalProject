9fcca3d3dba155f9f7e3e21e7e74a055ca7dfb01
W. Aaron Morris <waaronmorris@gmail.com>
2020-07-24 12:43:46 -0700

    Calculating All Beta Vega

--- betaVegaCalc.m
+++ betaVegaCalc.m
@@ -1,13 +1,15 @@
-function [b, v] = betaVegaCalc(c, d, r, peakIndex, daysPrior)
+function [b, v] = betaVegaCalc(s, c, d, r, w, pop)
     v = [];
     b = [];
-    for t = peakIndex-daysPrior:peakIndex
+    for t = 1:w-1
         t2 = t+1;
+        s1 = s(t);
+        s2 = s(t2);
         i1 = c(t) - d(t) - r(t);
-        i2 = c(t2) - d(t2) - r(t2);
+        %i2 = c(t2) - d(t2) - r(t2);
         r2 = d(t2) + r(t2);
         r1 = d(t) + r(t);
         v = [v (r2-r1)/i1];
-        b = [b (i2 + v(t-49)*i1)/i1];
+        b = [b (s1-s2)*pop*1/i1*1/s1];
     end
 end
