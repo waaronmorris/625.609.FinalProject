3abf4203431d5bbe295abd43ade36d7c8cfc8dab
W. Aaron Morris <waaronmorris@gmail.com>
2020-07-28 16:30:35 -0700

    Parameters is the ODE function

--- odefit.m
+++ odefit.m
@@ -2,6 +2,6 @@ function [err] = odefit(exp_t,exp_y,p)
     [~,y] = ode45(@(t,y)odefun(t,y,p),exp_t,[5072000 0 0]); %initial conditions w/ 1 infected (i) 
     %err = sum((y(:) - exp_y).^2);   % This gives a lousy fit!
     
-    err = 0*norm((diff(y) - diff(transpose(exp_y)))) + 1*norm((y - transpose(exp_y))); 
+    err = .5*norm((diff(y) - diff(transpose(exp_y)))) + .5*norm((y - transpose(exp_y))); 
     %err = norm((y - exp_y)); 
 end
