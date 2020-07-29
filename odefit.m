a4997aef6dbd0d97a08389d49ab82dd6260e8f1f
W. Aaron Morris <waaronmorris@gmail.com>
2020-07-28 20:25:39 -0700

    Trying different error function

--- odefit.m
+++ odefit.m
@@ -1,7 +1,7 @@
 function [err] = odefit(exp_t,exp_y,p)
-    [~,y] = ode45(@(t,y)odefun(t,y,p),exp_t,[5072000 0 0]); %initial conditions w/ 1 infected (i) 
+    [~,y] = ode45(@(t,y)odefun(t,y,p),exp_t,[5072000-1 1 0]); %initial conditions w/ 1 infected (i) 
     %err = sum((y(:) - exp_y).^2);   % This gives a lousy fit!
-    
-    err = .5*norm((diff(y) - diff(transpose(exp_y)))) + .5*norm((y - transpose(exp_y))); 
+    t = transpose(exp_y);
+    err = norm(transpose(exp_y)) - norm(y); 
     %err = norm((y - exp_y)); 
 end
--- odefun.m
+++ odefun.m
@@ -1,8 +1,7 @@
 function dydt = odefun(~,y,p)
-         
-         s = y(1);
-         i = y(2);
-         r = y(3);
+    s = y(1);
+    i = y(2);
+    r = y(3);
        
     dydt = [-p(1) * s* i/(i+s+r);
              (p(1) * s * i/(i+s+r)) - (p(2) * i);
